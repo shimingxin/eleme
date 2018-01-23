e98d75572330821213feb52e26e3139f00244c60
shimingxin <604393158@qq.com>
2018-01-23 21:37:09 +0800

    goods better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}"
+            @click="selectMenu(index)">
           <span class="text">
             <v-icon v-show="item.type>0" :size="3" :index="item.type"></v-icon>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
@@ -22,12 +23,11 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="new-price">¥{{food.price}}</span>
-                  <span v-show="food.oldPrice" class="old-price">¥{{food.oldPrice}}</span>
+                  <span class="new-price">¥{{food.price}}</span><span v-show="food.oldPrice"
+                                                                      class="old-price">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -40,6 +40,7 @@
 
 <script type="text/ecmascript-6">
   import icon from 'components/icon/icon';
+  import BScroll from 'better-scroll';
 
   const ERR_OK = 0;
   export default {
@@ -50,19 +51,77 @@
     },
     data () {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex () {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let l1 = this.listHeight[i];
+          let l2 = this.listHeight[i + 1];
+          if (!l2) return i;
+          if (this.scrollY >= l1 && this.scrollY < l2) {
+//            console.log(this.scrollY, i, l1, l2);
+            return i;
+          }
+        }
+        return 0;
+      }
     },
     created () {
       this.$http.get('/api/goods').then(response => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          /*
+          你在Vue生命周期的created()钩子函数进行的DOM操作一定要放在Vue.nextTick()的回调函数中。
+          原因是什么呢，原因是在created()钩子函数执行的时候DOM 其实并未进行任何渲染，
+          而此时进行DOM操作无异于徒劳，所以此处一定要将DOM操作的js代码放进Vue.nextTick()的回调函数中。
+          与之对应的就是mounted钩子函数，因为该钩子函数执行时所有的DOM挂载和渲染都已完成，此时在该钩子函数中进行任何DOM操作都不会有问题 。
+          在数据变化后要执行的某个操作，而这个操作需要使用随数据改变而改变的DOM结构的时候，
+          这个操作都应该放进Vue.nextTick()的回调函数中。
+           */
+          this.$nextTick(() => {
+            this._initScroll();
+            this._calculateHeight();
+          });
         }
       });
     },
     components: {
       'v-icon': icon
+    },
+    methods: {
+      _initScroll () {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true // 手机端点击返回一次
+
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3 // 监控滚动的位置
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+//          console.log(this.scrollY);
+        });
+      },
+      _calculateHeight () {
+        let foodlist = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodlist.length - 1; i++) {
+          let item = foodlist[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      selectMenu (index) {
+        let foodlist = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodlist[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      }
     }
   };
 </script>
@@ -85,13 +144,20 @@
         height: 54px
         width: 56px
         line-height: 14px
-        margin: 0 12px
+        padding: 0 12px
         border-1px(rgba(7, 17, 27, 0.1))
         .text
           display: table-cell
           width: 56px
           vertical-align: middle
           font-size: 12px
+        &.current
+          position: relative
+          z-index: 10
+          margin-top: -1px
+          background-color: white
+          font-weight: 700
+          border-none()
     .foods-wrapper
       flex: 1
       .food-list
@@ -118,33 +184,33 @@
             flex: 1
             .name
               font-size: 14px
-              height :14px
+              height: 14px
               line-height: 14px
               color: rgb(7, 17, 27)
               padding-top: 2px
             .desc
               font-size: 10px
-              line-height: 10px
+              line-height: 12px
               color: rgb(147, 153, 159)
               margin: 8px auto
             .extra
               font-size: 10px
-              height :10px
+              height: 10px
               line-height: 10px
               color: rgb(147, 153, 159)
-              &:first-child
+              .count
                 margin-right: 12px
             .price
-              font-weight :700
+              font-weight: 700
               .new-price
-                font-size :14px
-                color :rgb(240,20,20)
-                line-height :24px
-                margin-right :8px
+                font-size: 14px
+                color: rgb(240, 20, 20)
+                line-height: 24px
+                margin-right: 8px
               .old-price
-                font-size :10px
-                color :rgb(147, 153, 159)
-                line-height :24px
-                text-decoration :line-through
+                font-size: 10px
+                color: rgb(147, 153, 159)
+                line-height: 24px
+                text-decoration: line-through
 
 </style>
